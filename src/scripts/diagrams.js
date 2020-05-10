b328d5c6471dc1b6f7f6c7b4e33b55d76e22453c
Elena-Kho <elho@mail.ru>
2020-05-10 20:35:56 +0300

    Реализует блок Skills на Vue.js

--- src/scripts/diagrams.js
+++ src/scripts/diagrams.js
@@ -1,6 +1,44 @@
-import Vue from "vue";
+import Vue from 'vue';
+
+const diagram = {
+  template: '#diagram',
+  props: ['skill'],
+  methods: {
+    drawColoredCircle() {
+      const circle = this.$refs['circle'];
+      const dashArray  = parseInt(getComputedStyle(circle).getPropertyValue('stroke-dasharray'));
+      const percent = (dashArray / 100) * (100 - this.skill.percent);
+
+      circle.style.strokeDashoffset = percent;
+    }
+  },
+  mounted() {
+    this.drawColoredCircle();
+  },
+};
+
+const diagramsRow = {
+  template: '#diagrams-row',
+  components: {
+    diagram,
+  },
+  props: ['category']
+};
 
 new Vue({
   el: '#diagrams-component',
-  template: '#diagrams-list'
+  template: '#diagrams-list',
+  components: {
+    diagramsRow,
+  },
+  data() {
+    return {
+      skills: []
+    }
+
+  },
+  created() {
+    const data = require('../data/skills.json');
+    this.skills = data;
+  }
 });
