5ac193c96312f5d84a96d4c4972b95d28f71c058
Elena-Kho <elho@mail.ru>
2020-05-17 10:25:42 +0300

    Исправляет фокусы форм и добавляет слайдер работ на vue.js

--- src/scripts/portfolio.js
+++ src/scripts/portfolio.js
@@ -10,10 +10,12 @@ const btns = {
 
 const thumbs = {
   template: '#portfolio-thumbs',
+  props: ['works']
 };
 
 const tags = {
   template: '#portfolio-tags',
+  props: ['tags'],
 };
 
 
@@ -23,6 +25,7 @@ const display = {
     thumbs,
     btns
   },
+  props: ['currentWork', 'works'],
 };
 
 const info = {
@@ -30,6 +33,12 @@ const info = {
   components: {
     tags
   },
+  props: ['currentWork'],
+  computed: {
+    tagsArray() {
+      return this.currentWork.skills.split(',');
+    }
+  }
 };
 
 
@@ -37,4 +46,41 @@ new Vue({
   el: '#slider-component',
   template: '#portfolio-container',
   components: {display, num, info},
+  data() {
+    return {
+      works: [],
+      currentIndex: 0,
+      currentSlide: {}
+    }
+  },
+  computed: {
+    currentWork() {
+      return this.works[this.currentIndex];
+      console.log(currentWork)
+    }
+  },
+  methods: {
+    handleSlide(dir) {
+      switch(dir) {
+        case 'next':
+          this.currentIndex++;
+          break;
+        case 'prev':
+          this.currentIndex--;
+          break;
+      }
+    },
+    makeArrReqImg(array) {
+      return array.map(item => {
+        const requirePic = require(`../images/content/${item.img}`);
+        item.img = requirePic;
+
+        return item;
+      })
+    }
+  },
+  created() {
+    const data = require('../data/portfolio.json');
+    this.works = this.makeArrReqImg(data);
+  }
 });
