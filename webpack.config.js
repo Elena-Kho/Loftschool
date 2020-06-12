f7b737fa2d7780403b43da3d54a2a8c855f9ada8
Elena-Kho <elho@mail.ru>
2020-06-12 12:25:00 +0300

    Переписывает publicPath в webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProductionBuild = argv.mode === "production";
-  const publicPath = './';
-
+  const publicPath = 'https://github.com/Elena-Kho/Loftschool/'
   const pcss = {
     test: /\.(p|post|)css$/,
     use: [
